a405888563865f8b46588a6c3a8fc9507c29be8a
Steve Jason Developer <mehmoodasgharch@gmail.com>
2024-09-06 16:02:10 +0400

    lfs added

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
                             <div class="item h-100 d-flex w-100 relative  overflow-hidden lg_video">
                                                                 
                                 <video class="w-100 h-100 object-fit full-el img c_mobile" playsinline="true" muted loop
-                                    autoplay src="assets/home_video/Studio-1.mp4">
+                                    autoplay src="assets/home_video/Studio.mp4">
                                     Your browser does not support the video tag.
                                 </video>
                                 <div class="video-background">
